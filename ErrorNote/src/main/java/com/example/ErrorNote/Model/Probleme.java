89148a90b049c8b91f6e28723c17880354aa9c3b
jssagara <jeansagara272@gmail.com>
2022-08-26 09:38:59 +0000

    Creation des des classe dans le modele

--- ErrorNote/src/main/java/com/example/ErrorNote/Model/Probleme.java
+++ ErrorNote/src/main/java/com/example/ErrorNote/Model/Probleme.java
@@ -1,4 +1,32 @@
 package com.example.ErrorNote.Model;
 
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@Table(name = "probleme")
 public class Probleme {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id_probleme;
+    private String titre;
+    private String description;
+    private String technologie;
+    private String methodologie;
+    private String etat;
+
+    @ManyToOne
+    @JoinColumn(name = "User_Problème")
+    private User user;
+
+    @OneToOne
+    @JoinColumn(name = "Solution_probleme")
+    private Solution solution;
+
+
 }
--- ErrorNote/src/main/java/com/example/ErrorNote/Model/Solution.java
+++ ErrorNote/src/main/java/com/example/ErrorNote/Model/Solution.java
@@ -1,4 +1,33 @@
 package com.example.ErrorNote.Model;
 
+import lombok.Data;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@Entity
+@Table(name = "solution")
 public class Solution {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id_solution;
+    private String description;
+    private String temps;
+    private String ressource;
+
+    @OneToMany (mappedBy ="solution")
+    List<Commentaire> commentaires = new ArrayList<>();
+
+    @ManyToOne
+    @JoinColumn(name = "User_Solution")
+    private User user;
+
+    @OneToOne
+    @JoinColumn(name = "Solution_probleme")
+    private Probleme probleme;
+
 }
