72bb1faa4061043cec4dbb973514461ad6663e14
jssagara <jeansagara272@gmail.com>
2022-08-26 09:38:59 +0000

    Creation des des classe dans le modele

--- ErrorNote/src/main/java/com/example/ErrorNote/Model/Commentaire.java
+++ ErrorNote/src/main/java/com/example/ErrorNote/Model/Commentaire.java
@@ -1,4 +1,24 @@
 package com.example.ErrorNote.Model;
 
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@Table(name = "commentaire")
 public class Commentaire {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+        private Long id_commentaire;
+    private String contenu;
+
+    @ManyToOne
+    @JoinColumn(name = "User_Commentaire")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "Solution_commentaire")
+    private Solution solution;
 }
