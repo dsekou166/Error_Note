20d7b26464da76b480b06321a66ba3e16fb3cbab
jssagara <jeansagara272@gmail.com>
2022-08-26 17:17:30 +0000

    mise a jour des classe commentaire et solution

--- ErrorNote/src/main/java/com/example/ErrorNote/Repositories/ProblemeRepository.java
+++
@@ -1,4 +0,0 @@
-package com.example.ErrorNote.Repositories;
-
-public class ProblemeRepository {
-}
--- ErrorNote/src/main/java/com/example/ErrorNote/Services/CommentaireService.java
+++ ErrorNote/src/main/java/com/example/ErrorNote/Services/CommentaireService.java
@@ -1,4 +1,11 @@
 package com.example.ErrorNote.Services;
 
+import com.example.ErrorNote.Model.Commentaire;
+
+import java.util.List;
+
 public interface CommentaireService {
+    Commentaire Creer (Commentaire commentaire);
+    List<Commentaire> lister();
+    String Supprimer(Long id_commentaire);
 }
